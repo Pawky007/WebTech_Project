e18ffbd0d54af98f425f28c25be5f2e1c39c735c
abrar <abrar@ABRARLAPTOP>
2025-09-09 20:23:34 +0600

    lorry list added

---
+++ Lorry_List.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="dashboad_style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <aside class="sidebar">
+        <img src="Image/Logo.png" alt="HaulPro Logo" width="200px" />
+        <h3>HaulPro</h3>
+        <ul class="menu">
+          <li><i class="fa fa-tachometer-alt"></i> Dashboard</li>
+          <li><i class="fa fa-chart-line"></i> Analytics</li>
+          <li><i class="fa fa-bullhorn"></i> Banner</li>
+          <li><i class="fa fa-truck"></i> Vehicle</li>
+          <li><i class="fa fa-cogs"></i> Load Order</li>
+          <li><i class="fa fa-credit-card"></i> Payment Method</li>
+          <li><i class="fa fa-users"></i> Transporter List</li>
+          <li><i class="fa fa-user-tie"></i> Lorry Owner List</li>
+          <li><i class="fa fa-truck-moving"></i> Lorry List</li>
+          <li><i class="fa fa-cogs"></i> Setting</li>
+          <li><i class="fa fa-question-circle"></i> FAQ</li>
+        </ul>
+        <div class="help-card">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/4712/4712002.png"
+            alt="Help"
+          />
+          <p>Need Help?</p>
+          <button>Contact Now</button>
+        </div>
+      </aside>
+</body>
